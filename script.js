dc06964dc569ade8fa1cb52ea6f054049b7f61f7
Charlie Yalf <jordanandrewmorris12@gmail.com>
2024-05-27 18:45:47 +1000

    Banner & Nav Changes

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function toggleMobileMenu() {
-	const mobileMenu = document.getElementById('mobile-menu');
-	if (mobileMenu.classList.contains('hidden')) {
-		mobileMenu.classList.remove('hidden');
-	} else {
-		mobileMenu.classList.add('hidden');
-	}
+  const mobileMenu = document.getElementById("mobile-menu");
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.remove("hidden");
+  } else {
+    mobileMenu.classList.add("hidden");
+  }
+  const button = document.querySelector('button[aria-controls="mobile-menu"]');
+  const isExpanded = button.getAttribute("aria-expanded") === "true";
+
+  if (isExpanded) {
+    button.setAttribute("aria-expanded", "false");
+    button.classList.remove("active-link", "custom-hover-yellow");
+    button.classList.add("bg-white", "custom-hover-white");
+  } else {
+    button.setAttribute("aria-expanded", "true");
+    button.classList.add("active-link", "custom-hover-yellow");
+    button.classList.remove("bg-white", "custom-hover-white");
+  }
 }
